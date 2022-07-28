b59b1b43a9da75a396b5585f24db7f00819f5a85
João Gabriel Mortati Wiggert Diamantino <98192765+gabrielwiggert@users.noreply.github.com>
2022-07-28 18:41:30 -0300

    Update exercicio.sql
    ex. 02

--- exercicio.sql
+++ exercicio.sql
@@ -3,3 +3,9 @@
 SELECT "userId" as id, COUNT("endDate") as experiences
 FROM experiences
 GROUP BY "userId"
+
+--EX. 02
+
+SELECT "userId" as id, COUNT("userId") as educations
+FROM educations
+GROUP BY "userId"
