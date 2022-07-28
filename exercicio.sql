ffb472100a12df3fc17d3199baa1c24afee91bf9
João Gabriel Mortati Wiggert Diamantino <98192765+gabrielwiggert@users.noreply.github.com>
2022-07-28 19:08:29 -0300

    Update exercicio.sql

--- exercicio.sql
+++ exercicio.sql
@@ -1,11 +1,29 @@
 --EX. 01
 
-SELECT COUNT("endDate") as experiences
+SELECT COUNT("endDate") as "currentExperiences"
 FROM experiences
-GROUP BY "userId"
+GROUP BY "userId";
 
 --EX. 02
 
 SELECT "userId" as id, COUNT("userId") as educations
 FROM educations
-GROUP BY "userId"
+GROUP BY "userId";
+
+--EX 03
+
+SELECT users.name as writer, COUNT("writerId") as "testimonialCount"
+FROM testimonials
+JOIN users
+ON user.id="writerId"
+WHERE "writerId"=435
+GROUP BY "writerId";
+
+--EX. 04
+
+SELECT * FROM jobs j
+JOIN roles r
+ON j."roleId"=r.id
+WHERE j.active=true
+GROUP BY r.name
+ORDER BY "maximumSalary";
